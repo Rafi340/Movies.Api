7206b005fc3866b8b64aa3f761f9b82d01d6e816
Rafi Samnan <rafi.samnan@savethechildren.org>
2025-07-27 16:36:16 +0600

    refit base sdk

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+using System.Text.Json;
+
+var moviesApi = RestService.For<IMoviesApi>("https://localhoast:5003");
+
+var movie = await moviesApi.GetMovieAsync("some-movie-id-or-slug");
+
+var request = new GetAllMoviesRequest { Page = 1, PageSize = 10,
+    Title = null,
+    YearOfRelease = null,
+    SortBy = null
+};
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
+
+
---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,20 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Sdk
+{
+    public interface IMoviesApi
+    {
+        [Get(ApiEndPoints.Movies.Get)]
+        Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+        [Get(ApiEndPoints.Movies.GetAll)]
+        Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
+    }
+}
